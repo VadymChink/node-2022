b175e71f33198df403f130e8c2ba8abe129bc42d
VadymChink <vadim-vsnnschuk@ro.ru>
2022-06-20 17:59:41 +0300

    hw_2.6

--- routes/use.router.js
+++ routes/use.router.js
@@ -3,9 +3,15 @@ const router = require('express').Router();
 const {userController} = require("../controllers");
 const {commonMdlwr, userMdlwr} = require("../middlewares");
 
-router.get('/',userController.findAllUsers);
-router.post('/',userMdlwr.isValidUserForCreate, userMdlwr.isUserUniq,userController.createUser);
-router.get('/:userId',commonMdlwr.isValidId,userController.findOneUser);
+router.get('/',
+    userController.findAllUsers);
+router.post('/',
+    userMdlwr.isValidUserForCreate,
+    userMdlwr.isUserUniq,
+    userController.createUser);
+router.get('/:userId',
+    commonMdlwr.isValidId,
+    userController.findOneUser);
 router.delete('/:userId',
     commonMdlwr.isValidId,
     userMdlwr.isUserPresent,
