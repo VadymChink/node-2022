83077639f8d37d64a035c9f86c2ee797f8597638
VadymChink <vadim-vsnnschuk@ro.ru>
2022-06-25 10:36:09 +0300

    token

--- routes/users.route.js
+++ routes/users.route.js
@@ -2,11 +2,11 @@ const usersRoute = require('express').Router();
 const {usersController} = require('../controllers');
 const userMdlwr = require('../middlewares/user.middleware');
 
-usersRoute.get('/', usersController.getAllUsers)
-usersRoute.post('/', userMdlwr.checkUserOnCreate, usersController.createUser)
-usersRoute.get('/:userId',userMdlwr.checkIdOnValid, usersController.getById)
-usersRoute.delete('/:userId',userMdlwr.checkIdOnValid, usersController.deleteById)
-usersRoute.put('/:userId',userMdlwr.checkIdOnValid, usersController.updateById)
+usersRoute.get('/', usersController.getAllUsers);
+usersRoute.post('/', userMdlwr.checkUserOnCreate, usersController.createUser);
+usersRoute.get('/:userId',userMdlwr.checkIdOnValid, usersController.getById);
+usersRoute.delete('/:userId',userMdlwr.checkIdOnValid, usersController.deleteById);
+usersRoute.put('/:userId',userMdlwr.checkIdOnValid, usersController.updateById);
 
 module.exports = {
     usersRoute,
